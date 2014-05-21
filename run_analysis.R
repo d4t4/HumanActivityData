61374abd6440bcc10f0c710c9a96af6e7f9fd976
D4t4 Student <d4t4.student@gmail.com>
2014-05-21 17:08:51 -0400

    Initial commit of base files and directory structure

---
+++ run_analysis.R
@@ -0,0 +1,5 @@
+## Load test and training data
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "Dataset.zip", method = "curl")
+unzip("Dataset.zip")
